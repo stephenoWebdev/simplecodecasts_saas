e7589b9e0ece1fb1bdf8fed28e85b7a3b12d0f8c
Stephen O'Callaghan <stepheno.webdev@gmail.com>
2015-08-07 10:48:46 +0000

    Checking contact form email heroku errors

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,23 +3,20 @@ class ContactsController < ApplicationController
      @contact = Contact.new
   end
   
-  def create
-    @contact = Contact.new(contact_params)
-    
-    if @contact.save
-      name = params[:contact][:name]
-      email = params[:contact][:email]
-      body = params[:contact][:body]
-      
-      ContactMailer.contact_email(name,email,body).deliver
-      
-      flash[:success] = "Message Sent"
-      redirect_to new_contact_path
-    else
-      flash[:danger] = "Error occured, message has not been sent"
-      redirect_to new_contact_path
-    end
+def create
+  @contact = Contact.new(contact_params)
+  if @contact.save
+    name = params[:contact][:name]
+    email = params[:contact][:email]
+    body = params[:contact][:comments]
+    ContactMailer.contact_email(name, email, body).deliver
+    flash[:success] = 'Message sent.'
+    redirect_to new_contact_path
+  else
+    flash[:danger] = 'Error occured, message has not been sent.'
+    redirect_to new_contact_path
   end
+end
   
   private
     def contact_params

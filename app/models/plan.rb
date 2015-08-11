5883d168569326f7b0e9fc38b0723fe752a45643
Stephen O'Callaghan <stepheno.webdev@gmail.com>
2015-08-11 07:20:51 +0000

    correcting db issues

---
+++ app/models/plan.rb
@@ -0,0 +1,3 @@
+class Contact < ActiveRecord::Plan
+    
+end
--- test/helpers/pages_helper_test.rb
+++ test/helpers/pages_helper_test.rb
@@ -1,4 +1,9 @@
+ActiveRecord::Migration.maintain_test_schema!
+
 require 'test_helper'
+
+
+
 
 class PagesHelperTest < ActionView::TestCase
 end
